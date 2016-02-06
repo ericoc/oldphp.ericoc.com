5509b4e99b84cc29bfd37c03380fdcf0aa89aceb
Eric OC <eric@ericoc.com>
2016-02-06 11:01:02 -0500

    tab-out HTML for pretty source view, remove meta robots ignore tag, and close <form> tag for search box

--- indego.php
+++ indego.php
@@ -129,58 +129,57 @@ function make_graph($bikes, $docks) {
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<meta name="robots" content="noindex,nofollow">
-<title>Indego Bikes!</title>
-<style type="text/css">
-<!--
-.header {
-	font-weight: bold;
-	border: 1px solid #000;
-	font-family: Tahoma, sans-serif;
-}
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>Indego Bikes!</title>
+		<style type="text/css">
+		<!--
+			.header {
+				font-weight:	bold;
+				border:		1px solid #000;
+				font-family:	Tahoma, sans-serif;
+			}
 
-table, tr, td {
-	border-collapse: collapse;
-	border: 1px dotted #000;
-}
+			table, tr, td {
+				border-collapse:	collapse;
+				border:			1px dotted #000;
+			}
 
-tr:nth-child(2n) {
-	background-color: #eee;
-}
+			tr:nth-child(2n) {
+				background-color:	#eee;
+			}
 
-tr:target {
-	background-color: orange;
-}
+			tr:target {
+				background-color:	orange;
+			}
 
-.bikes {
-	color: #16216a;
-}
+			.bikes {
+				color:	#16216a;
+			}
 
-.docks {
-	color: #777;
-}
+			.docks {
+				color:	#777;
+			}
 
-h1 {
-	color: #16216a;
-	text-decoration: underline;
-	font-family: Tahoma, sans-serif;
-}
+			h1 {
+				color:			#16216a;
+				text-decoration:	underline;
+				font-family:		Tahoma, sans-serif;
+			}
 
--->
-</style>
-</head>
-<body>
-<h1><a href='<?php echo $_SERVER['PHP_SELF']; ?>'>Indego Bikes</a></h1>
-<table>
-<tr class='header'>
-<td>Kiosk #</td>
-<td>Name</td>
-<td>Bikes</td>
-<td></td>
-<td>Docks</td>
-</tr>
+		-->
+		</style>
+	</head>
+	<body>
+		<h1><a href='<?php echo $_SERVER['PHP_SELF']; ?>'>Indego Bikes</a></h1>
+		<table>
+			<tr class='header'>
+				<td>Kiosk #</td>
+				<td>Name</td>
+				<td>Bikes</td>
+				<td></td>
+				<td>Docks</td>
+			</tr>
 <?php
 
 // Use search term if one was given, otherwise blank returns all stations
@@ -209,13 +208,13 @@ foreach ($stations as $station) {
 	$address = $station->addressStreet . ' (' . $station->addressZipCode . ')';
 
 	// List the current stations information in a unique table row
-	echo "<tr id='$station->kioskId'>\n";
-	echo "<td><a href='#$station->kioskId'>$station->kioskId</a></td>\n";				// Anchor link to the station/kiosk IDs
-	echo "<td><span title='$address'>$station->name</span></td>\n";					// Hover text on the name shows address+zip code, but doesn't work on mobile :/
-	echo "<td>$station->bikesAvailable</td>\n";							// Number of bikes available at the station
-	echo "<td>" . make_graph($station->bikesAvailable, $station->docksAvailable) . "</td>\n";	// Generate and show pretty graph of bikes vs. docks at the station
-	echo "<td>$station->docksAvailable</td>\n";							// Number of docks available at the station
-	echo "</tr>\n";
+	echo "			<tr id='$station->kioskId'>\n";
+	echo "				<td><a href='#$station->kioskId'>$station->kioskId</a></td>\n";				// Anchor link to the station/kiosk IDs
+	echo "				<td><span title='$address'>$station->name</span></td>\n";				// Hover text on the name shows address+zip code, but doesn't work on mobile :/
+	echo "				<td>$station->bikesAvailable</td>\n";							// Number of bikes available at the station
+	echo "				<td>" . make_graph($station->bikesAvailable, $station->docksAvailable) . "</td>\n";	// Generate and show pretty graph of bikes vs. docks at the station
+	echo "				<td>$station->docksAvailable</td>\n";							// Number of docks available at the station
+	echo "			</tr>\n";
 
 	// Add the current stations counts to the totals
 	$totalbikes += $station->bikesAvailable;
@@ -225,28 +224,31 @@ foreach ($stations as $station) {
 
 // Show a nice message if no active stations instead of leaving empty table
 if ($totalstations == 0) {
-	echo "<tr><td align='center' colspan='5'><i>No active stations found!</i><td></tr>\n";
+	echo "			<tr>\n";
+	echo "				<td align='center' colspan='5'><i>No active stations found!</i><td>\n";
+	echo "			</tr>\n";
 }
 
 // Show the total counts at the bottom of our table
-echo "<tr class='header'>\n";
-echo "<td>Totals</td>\n";
-echo "<td>$totalstations stations</td>\n";
-echo "<td>$totalbikes</td>\n";
-echo "<td></td>\n";
-echo "<td>$totaldocks</td>\n";
-echo "</tr>\n";
+echo "			<tr class='header'>\n";
+echo "				<td>Totals</td>\n";
+echo "				<td>$totalstations stations</td>\n";
+echo "				<td>$totalbikes</td>\n";
+echo "				<td></td>\n";
+echo "				<td>$totaldocks</td>\n";
+echo "			</tr>\n";
 
 // Yay - search form and links below!
 ?>
-</table>
-<br>
-<form method="get">
-<input type="text" name="search"> <i>(i.e. "<a href="?search=fairmount">fairmount</a>" or "<a href="?search=19107">19107</a>")</i>
-<input type="submit" value="Search!">
-<pre>
-courtesy of <a href='https://www.rideindego.com/stations/json/' target='_blank'>https://www.rideindego.com/stations/json/</a><br>
-<a href='https://github.com/ericoc/ericoc.com/blob/master/indego.php' target='_blank'>view source @ github</a> | <a href='https://github.com/ericoc/indego-php-lib' target='_blank'>my Indego PHP library</a>
-</pre>
-</body>
+		</table>
+		<br>
+		<form method='get'>
+		<input type='text' name='search'> <i>(i.e. "<a href='?search=fairmount'>fairmount</a>" or "<a href='?search=19107'>19107</a>")</i>
+		<input type='submit' value='Search!'>
+		</form>
+		<pre>
+			courtesy of <a href='https://www.rideindego.com/stations/json/' target='_blank'>https://www.rideindego.com/stations/json/</a><br>
+			<a href='https://github.com/ericoc/ericoc.com/blob/master/indego.php' target='_blank'>view source @ github</a> | <a href='https://github.com/ericoc/indego-php-lib' target='_blank'>my Indego PHP library</a>
+		</pre>
+	</body>
 </html>
